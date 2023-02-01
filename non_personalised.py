2e799878f1c79ef44c14fc5eea3e1b0833fbe9e6
Mingyue Jian <71535426+mingyeahh@users.noreply.github.com>
2023-02-01 01:15:36 +0000

    finalised non-personalised system

---
+++ non_personalised.py
@@ -0,0 +1,97 @@
+from dataprocessing import *
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+TOP = 30
+# Good rate threshold
+GOOD = 7
+
+
+############## The basic idea of the non-personalised recommender system  ##############
+############## 1. the average ratings across users                        ##############
+############## 2. watched time by users                                   ##############
+############## 3. the number of genres the movie includes                 ##############
+############## -> all of these are generic features                       ##############
+
+
+''' The system for non-personalised recommender system '''
+
+def nps(df, top):
+    # ------ Build feature data --------
+    # 1. Get the averaage rating  for each film
+    average_rating = df.groupby('movieId')['rating'].mean()
+    df['averageRating'] = df['movieId'].map(average_rating)
+
+    # 2. Get the number of rating for each film
+    counts = train['movieId'].value_counts()
+    df['rateTime'] = df['movieId'].map(counts)
+
+    # 3. Get the number of genres for each film
+    df['genresCount'] = df['genres'].str.count('\\|') + 1
+    
+    # Visualisation of features that have the same values -> reason why muilti-layers of sorting is needed
+    # ar = 5855 - len(df[['averageRating']].value_counts())
+    # print(f'{ar} films that have the same average rating.\n')
+    # rt = 5855 - len(df[['averageRating', 'rateTime']].value_counts())
+    # print(f'{rt} films that have the same average rating and the same number of rate time.\n')
+    # gc = 5855 - len(df[['averageRating', 'rateTime', 'genresCount']].value_counts())
+    # print(f'{gc} films that have the same average rating, the same number of rate time and the same number of genres.\n')
+    
+    # ------- Dataset cleaning -------
+    # Drop columns that will affect further duplicate removing
+    df = df.drop(['genres', 'rating', 'userId'], axis=1)
+
+    # Drop duplicates in the dataset
+    df = df.drop_duplicates()
+
+    # Sort the dataset by the averageRating, ratetime and number of genres in order.
+    df = df.sort_values(['averageRating', 'rateTime', 'genresCount'], ascending=[False, False, False])
+    
+    # Get the top films from the sorted list for recommendation
+    r = df.head(top)
+    return r
+
+
+
+
+'''System Evaluation'''
+# Build evaluation dataframe
+# The Idea is to apply different metrics for evaluation. Metrics used: MSE, RMSE, Recall30 and Precision
+
+# Building train and testing set for further evaluation
+train_users, test_users = train_test_split(np.arange(n_userIds), test_size=0.3, random_state=3)
+train = dfn[dfn['userId'].isin(train_users)].copy()
+test = dfn[dfn['userId'].isin(test_users)].copy()
+
+# Get the list of top recommendations
+r = nps(train, TOP)
+
+movie_list = r['movieId'].values
+
+# Get rows that has the same movie id as the recommended 30 films
+e_df = test.loc[test['movieId'].isin(movie_list)]
+
+e_df = e_df.drop(['title', 'genres'], axis = 1)
+
+# Make a new column for predicted rating
+e_df['pred'] = e_df['movieId'].map(average_rating)
+
+# Evaluation in the metric of mse and rmse
+mse = mean_squared_error(e_df['rating'].values, e_df['pred'].values)
+rmse = np.sqrt(mse)
+
+# Evaluation in the metric of precision and recall30
+all_rec = len(e_df)
+tp = len(e_df[(e_df['rating'] >= GOOD) & (e_df['pred'] >= GOOD)])
+precision =  tp / all_rec
+
+gt_good = len(test[(test['rating'] >= GOOD)])
+recall = tp / gt_good
+
+print('-- Evaluation for non-psersonalised recommender system --')
+print(f'Recommendation number is {TOP}')
+print(f'MSE: {mse}')
+print(f'RMSE: {rmse}')
+print(f'percision: {precision}')
+print(f'recall: {recall}')
--- user.py
+++ user.py
@@ -1,40 +1,46 @@
+from non_personalised import nps
 print('Loading...\n')
-from dataprocessing import *
 
 # Get the movie id from the movie input by the user
-def getMovieid(movieStr):
-    while True:
-        # Check whether the dataset contains the input string as a substring
-        if (dfm['title'].str.contains(movieStr)).any():
-            selection = dfm[dfm['title'].str.contains(movieStr)]
-            print('There is a list of movies from the dataset based on your input.')
-            print('Please choose one of them and \033[4menter the movie ID\033[0m:\n')
-            print(f"{'ID':>4} | Title")
-            print("-----+"+'-'*20)
-            for i, row in selection.iterrows():
-                print(f"{row['movieId']:>4} | {row['title']}")
-                print('  ')
-            movieid = input("\nEnter the movieid: ")
-            while True:
-                # Check whether the input movie id is valid -> is an integer and is on the given list
-                if movieid.isnumeric() and int(movieid) in selection['movieId'].values:
-                    return int(movieid)
-                else:
-                    movieid = input('Please enter a valid movieId on the given list: ')
+# def getMovieid(movieStr):
+#     while True:
+#         # Check whether the dataset contains the input string as a substring
+#         if (dfm['title'].str.contains(movieStr)).any():
+#             selection = dfm[dfm['title'].str.contains(movieStr)]
+#             print('There is a list of movies from the dataset based on your input.')
+#             print('Please choose one of them and \033[4menter the movie ID\033[0m:\n')
+#             print(f"{'ID':>4} | Title")
+#             print("-----+"+'-'*20)
+#             for i, row in selection.iterrows():
+#                 print(f"{row['movieId']:>4} | {row['title']}")
+#                 print('  ')
+#             movieid = input("\nEnter the movieid: ")
+#             while True:
+#                 # Check whether the input movie id is valid -> is an integer and is on the given list
+#                 if movieid.isnumeric() and int(movieid) in selection['movieId'].values:
+#                     return int(movieid)
+#                 else:
+#                     movieid = input('Please enter a valid movieId on the given list: ')
 
-        else:
-            print('Sorry, the input movie is not in our database :(. ')
-            print('Please check whether you have typed in the correct movie name or try another movie?')
+#         else:
+#             print('Sorry, the input movie is not in our database :(. ')
+#             print('Please check whether you have typed in the correct movie name or try another movie?')
 
-            movieStr = input("\nEnter the film name: ")
+#             movieStr = input("\nEnter the film name: ")
             
-# Check whether the input user id is valid            
+# Check whether the input user id is valid (is numeric and is in the database)            
 def checkUserid(userid):
     while True:
         if userid.isnumeric() and int(userid) in dfr['userId'].values:
             return int(userid)
         else:
             userid = input('Please enter a valid userId: ')
+
+# User getting recommendation
+def show_rec(df):
+    rl = df['title'].values
+    for i in range(len(rl)):
+        print(f'[{i+1}]:  {rl[i]}')
 
 # Intro to users about the system
 print('~~ Hello there! Welcome to use this movie recommender system! Meow~~')
@@ -48,10 +54,11 @@ print('-------------------------------------------------------------------------
 
 while True:
     if login_action == 'A':
-        print('⭐ The system applies a pre-trained neural collaborative filtering model for movie recommendation,')
-        print('which means it gives prediction by comparing your past preference(movie ratings) with other users who are similar to you and recommend to you some movies these users rank high :D! Meow~\n')
-        print('⭐ Of course! If you don\'t want us to use your data, you can use the non-personalised system!')
-        print('⭐ You can either tell us what type of film you want use to recommend or tell us the name of the past film you\'ve watched and let us recommend similar films to you.')
+        print('🐱: The system offered two type of movie recommendation: a personalised one and a non-personalised one.')
+        print('🐱: The personalised system applies a pre-trained neural collaborative filtering model for movie recommendation,')
+        print('which means it gives prediction by comparing your past preference(movie ratings) with other users who are similar to you and recommend to you some movies these users rank high :D! Meow~ Thus, you will need to log in with your userid so we can store your user preference.\n')
+        print('🐱: Of course! If you don\'t want your data to be used, you can use the non-personalised system, which will give recommendation according to the average ranking, number of watches and number of genre of movies in our database. Top 30 movies will be recommended according to the conditions mentioned before.')
+        print('🐱: Hope you like the meowie recommender system! Meow!')
         print('\n')
         input("Press Enter to continue :")
         login_action = 'B'
@@ -66,9 +73,9 @@ while True:
         
 print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
 print('-----------------------------------------------------------------------------------------')
-print('To use the system, you need to select a recommendation type first. Meow~')
+print('🐱: To use the system, you need to select a recommendation type first. Meow~')
 print('- For a personalised one, you need to login so we could use your past ratings to predict what you might like among the films you haven\'t watched before!')
-print('- For a non-personalised one, you don\'t need to log in, the recommendation will only be based on the input film, and find the similar films for you.\n')
+print('- For a non-personalised one, you don\'t need to log in, the recommendations are selected by the system based on the database.\n')
 print('Please choose the recommendation type:')
 print('(A) Personalised recommendation. Log-in required.')
 print('(B) Non-personalised recommendation. Log-in not required.')
@@ -87,11 +94,13 @@ while True:
 
 
     elif r_type == 'B':
-        print('Please enter a film you like:')
-        film = input("\nEnter the film name: ")
-        m = getMovieid(film)
-        print('Movie id is:',m)
+        # Building dataset for non-personalised system
+        dfn = pd.merge(dfr, dfm, on='movieId')
+        r = nps(dfn, TOP)
+        show_rec(df)
         break
+        
+        
 
     else:
         print('Please enter a correct input.')
